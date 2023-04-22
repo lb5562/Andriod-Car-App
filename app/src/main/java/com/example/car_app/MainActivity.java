c3784c5781169d0304b15cd7d047fc7ce9a9cd16
lb5562 <lucille_blain@live.com>
2023-04-22 09:31:00 -0400

    added connect button and checkers

--- app/src/main/java/com/example/car_app/MainActivity.java
+++ app/src/main/java/com/example/car_app/MainActivity.java
@@ -1,5 +1,14 @@
 package com.example.car_app;
 
+import android.annotation.SuppressLint;
+import android.app.Instrumentation;
+import android.app.PendingIntent;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -9,6 +18,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -18,11 +28,21 @@ import com.example.car_app.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity implements JoyStickListener {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    private UsbManager usbManager;
+    private BluetoothDevice adino;
+    private Boolean connection;
+    private int DEVICE_ADDRESS =0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +72,6 @@ public class MainActivity extends AppCompatActivity implements JoyStickListener 
     }
 
 
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -75,6 +93,36 @@ public class MainActivity extends AppCompatActivity implements JoyStickListener 
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void onClickConnect(View view) {
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter == null) {
+            Toast.makeText(getApplicationContext(), "Device doesnt Support Bluetooth", Toast.LENGTH_SHORT).show();
+
+        } else {
+            if (!adapter.isEnabled()) {
+                Intent enable = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                registerForActivityResult(enable, new Intent(BluetoothAdapter.STATE_DISCONNECTED));
+            } else {
+                }
+                @SuppressLint("MissingPermission") Set bonding = adapter.getBondedDevices();
+                if(bonding.isEmpty()){
+                    Toast.makeText(getApplicationContext(),"Please Pair the Device",Toast.LENGTH_SHORT).show();
+                }else{
+                    for(Object it:bonding){
+                        if(it instanceof  BluetoothDevice){
+                        if(((BluetoothDevice) it).getAddress().equals(DEVICE_ADDRESS){
+                            adino = (BluetoothDevice) it;
+                            connection = true;
+                            break;
+                        }
+                    }
+                    }
+                }
+
+            }
+
+        }
 
     @Override
     public boolean onSupportNavigateUp() {
